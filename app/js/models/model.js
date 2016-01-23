5ac51ced6cbeec81fcb72a95a91eaa756ad1e48f
Anna Krivova <ani94@mail.bg>
2016-01-23 17:56:56 +0200

    js model with get and set functions + local storage

--- app/js/models/model.js
+++ app/js/models/model.js
@@ -1,7 +1,37 @@
-(function(app){
+/**
+ * Application model
+ */
+
+var app = window.app || {};
+
+(function () {
 
     app.model = {
 
-    }
+        get: function(key){
+            var value = localStorage.getItem(key);
 
-})(window.app || {});
+            if (!value) {return;}
+
+            // in case it is an object that has been stringified
+            if (value[0] === "{") {
+              value = JSON.parse(value);
+            }
+
+            return value;
+        },
+
+        set: function(key, value){
+            if (!key || !value) {return;}
+
+            if (typeof value === "object") {
+              value = JSON.stringify(value);
+            }
+            localStorage.setItem(key, value);
+        },
+
+        parse: function(obj){
+
+        }
+    };
+})();
