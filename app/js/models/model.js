93c85e41ebbb3eddf4279be77fb15c5b8a481057
Anna Krivova <ani94@mail.bg>
2016-02-21 11:20:28 +0200

    model

--- app/js/models/model.js
+++ app/js/models/model.js
@@ -9,10 +9,41 @@ var app = window.app || {};
     app.model = {
 
         /**
+         * Settings object - keeps image properties
+         * @type {Object}
+         */
+        settings: {
+            scaled : {
+                width : 'width:' + widthValue,
+                height : 'height' + heightValue
+            },
+            translated: {
+                x : x,
+                y : y,
+                z : z
+            },
+            rotated : 'rotate('+deg+'deg)',
+            fliped : {
+
+                horizontalFront : 'transform: rotateX(180deg)',
+                horizontalBack : 'transform: rotateX(-180deg)',
+                verticalFront : 'transform: rotateY(180deg)',
+                verticalBack : 'transform: rotateY(-180deg)'
+
+            },
+            moved : {
+                left : 'translate('+x+','+y+')',
+                right: 'translate('+x+','+y+')',
+                up : 'translate('+x+','+y+')',
+                down : 'translate('+x+','+y+')'
+            }
+        },
+
+        /**
          * @param  {String} key
          * @return {Object} value
          */
-        get: function(key){
+        get: function(key) {
 
             var value = JSON.parse(localStorage.key);
 
@@ -23,15 +54,14 @@ var app = window.app || {};
          * @param {String} key
          * @param {Object} obj
          */
-        set: function(key,obj){
+        set: function(key,obj) {
 
             var  value = JSON.stringify(obj);
-
             localStorage.setItem(key,value);
 
         },
 
-        parse: function(obj){
+        parse: function(obj) {
 
         }
     };
