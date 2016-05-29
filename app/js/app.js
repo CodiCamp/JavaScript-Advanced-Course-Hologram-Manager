0c5a73a7f5e632164e06d2b6229f041f2f85fbb9
Ani Krivova <anjelika.k08@gmail.com>
2016-05-29 21:03:37 +0300

    Added Register View

--- app/js/app.js
+++ app/js/app.js
@@ -36,7 +36,7 @@
     var files = {
         templates: ['templates.js'],
         components: ['initable.js', 'generic-view.js', 'events.js'],
-        views: ['main-view.js', 'login-view.js', 'presets-view.js', 'settings-view.js', 'edit-view.js'],
+        views: ['main-view.js', 'login-view.js','register-view.js', 'presets-view.js', 'settings-view.js', 'edit-view.js'],
         modules: ['state.js'],
         models:['model.js','firebase.js','localstorage.js']
     };
